56a0ebe3b9cef83cb1cfd6cf7e16eef6fdddffc5
dianasanjuan0017 <20230019@uthh.edu.mx>
2024-06-22 22:06:41 -1100

    modified:   formulario.php 	modified:   index.html

--- formulario.php
+++ formulario.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
   
    if (empty($usuario) || empty($password) || empty($edad)) {
-       echo  "Todos los campos son obligatorios";
+       echo  "Todos los campos deben estar llenos";
    }
 
    if (!is_numeric($edad) || $edad < 18) {
